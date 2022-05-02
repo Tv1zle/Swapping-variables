97b36eb62d1999e19070813868002c8fb95d844f
User <User@WIN-D6I7BGDFHRA>
2022-05-02 19:18:07 +0200

    Добавьте файлы проекта.

---
+++ ClassWork031.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    //С третьей переменной
+    int a = 10;
+    int b = 15;
+    
+    int c = 0;
+
+    c = a;
+    a = b;
+    b = c;
+
+    std::cout << a << " " << b << "\n";
+
+    //Без третьей переменной
+    a = 10;
+    b = 15;
+
+    a = a + b;
+    b = b - a;
+    b = -b;
+    a = a - b;
+
+    std::cout << a << " " << b << "\n";
+}
